f17e935a705a2bbcc2afa235dfbc3bcebcc4817e
Gabriele <gabriele.manfredi.999@gmail.com>
2017-06-29 23:19:02 +0200

    initial commit
    	modified:   .gitignore
    	new file:   .idea/workspace.xml
    	new file:   programmazione-java-esercizi.iml
    	new file:   src/it/madlabs/fc/model/Person.java

---
+++ src/it/madlabs/fc/model/Person.java
@@ -0,0 +1,24 @@
+package it.madlabs.fc.model;
+/**
+ * Created by gabriele on 29/06/17.
+ */
+public class Person {
+    private String name;
+    private String lastame;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastame() {
+        return lastame;
+    }
+
+    public void setLastame(String lastame) {
+        this.lastame = lastame;
+    }
+}
