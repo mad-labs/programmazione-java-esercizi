d44dfc7125ee1046c494a3f7db1c3bd979fdf2b0
gabriele <gabriele.manfredi@hotmail.it>
2017-07-25 18:20:56 +0200

    new file:   src/test/java/it/madlabs/PplayGroung.java

---
+++ src/test/java/it/madlabs/PplayGroung.java
@@ -0,0 +1,12 @@
+package it.madlabs;
+
+/**
+ * Created by gabriele on 13/07/17.
+ */
+public class PlayGround {
+
+    public static void main(String[] args) {
+        String cisao;
+        System.out.println("ciasao: "+cisao);
+    }
+}
